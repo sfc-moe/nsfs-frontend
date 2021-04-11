e8ca254c1f1705dc4b6cfc7e32b0fca049235389
Delton Ding <dsh0416@gmail.com>
2021-04-11 22:54:48 +0800

    Fix builds

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,3 +3,17 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare type Nullable<T> = T | null;
+
+declare type CustomizedHTMLElement<T> = HTMLElement & T
+
+declare type Indexable<T> = {
+  [key: string]: T
+}
+
+declare type Hash<T> = Indexable<T>
+
+declare type TimeoutHandle = ReturnType<typeof global.setTimeout>
+
+declare type ComponentSize = 'large' | 'medium' | 'small' | 'mini'
