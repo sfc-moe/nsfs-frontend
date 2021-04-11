b397716f36130de0a815c2006187fe3d98ec2a46
Delton Ding <dsh0416@gmail.com>
2021-04-12 01:28:06 +0800

    Fix Lint

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,21 @@
 <template lang="pug">
 hello-world(:msg="$t('message.hello')")
 </template>
-  
-<script lang="ts">
-import { ref, defineComponent } from 'vue';
-import HelloWorld from '/@/components/HelloWorld.vue';
 
-export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-  setup: () => {
-    const count = ref(0);
-    return { count };
-  },
-});
+<script lang="ts">
+  import { ref, defineComponent } from 'vue';
+  import HelloWorld from '/@/components/HelloWorld.vue';
+
+  export default defineComponent({
+    name: 'Home',
+    components: {
+      HelloWorld,
+    },
+    setup: () => {
+      const count = ref(0);
+      return { count };
+    },
+  });
 </script>
-  
+
 <style scoped lang="stylus"></style>
-  
