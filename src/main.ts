7d65c5c01bec0918ef0320ee93af3068db7b403b
Delton Ding <dsh0416@gmail.com>
2021-04-12 15:11:35 +0800

    Add vue router (#1)
    * Add vue router
    
    * Fix Lint

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 import App from './App.vue';
 import i18n from './i18n';
+import router from './router';
 
 const app = createApp(App);
 app.use(i18n);
+app.use(router);
 app.use(ElementPlus, {
   i18n: i18n.global.t,
 });
